ad2569800bdea2e620c24a20831ee74c4872b7f6
Carlos Rivas <Cjrivas00@gmail.com>
2022-02-13 18:40:15 -0500

    getting function to display the current condtions on open weather app

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,25 @@
 let owmAPI = "788d5638d7c8e354a162d6c9747d1bdf";
 let currentCity = "";
 let lastCity = "";
+
+
+// this is an error handle for fetch 
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+// Function to get and display the current conditions on Open Weather Maps
+var getCurrentConditions = (event) => {
+    // Obtain city name from the search box
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+    // Set the queryURL to fetch from API using weather search - added units=imperial to fix
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+        return response.json();
+    })
